64d597862155843665c635a0829fe7683937973c
Theo Rode <theorodester@gmail.com>
2022-02-20 17:29:07 -0800

    fixed spelling errors

--- src/albums.jl
+++ src/albums.jl
@@ -19,9 +19,9 @@ Get an album: https://developer.spotify.com/documentation/web-api/reference/#/op
 
 """
 function get_album(spotifyDetails::SpotifyDetails, album_id::String; market::String="US")
-    urlextention = "albums/$(album_id)?market=$(market)" # get album extension 
+    urlextension = "albums/$(album_id)?market=$(market)" # get album extension 
 
-    spotify_request(spotifyDetails, urlextention) # make request
+    spotify_request(spotifyDetails, urlextension) # make request
 
 end # function get_album
 
@@ -36,9 +36,9 @@ function get_multiple_albums(spotifyDetails::SpotifyDetails, album_ids::Vector{S
 
     album_ids_uri = HTTP.escapeuri(album_idsString) # perform url encoding
 
-    urlextention = "albums?ids=$(album_ids_uri)&market=$(market)" # get albums extension 
+    urlextension = "albums?ids=$(album_ids_uri)&market=$(market)" # get albums extension 
 
-    spotify_request(spotifyDetails, urlextention) # make request
+    spotify_request(spotifyDetails, urlextension) # make request
 
 end # function get_multiple_albums
 
@@ -47,9 +47,9 @@ Get tracks for a specific album: https://developer.spotify.com/documentation/web
 
 """
 function get_album_tracks(spotifyDetails::SpotifyDetails, album_id::String; limit::Int=50, market::String="US", offset::Int=0)
-    urlextention = "albums/$(album_id)/tracks?limit=$(limit)&offset=$(offset)&market=$(market)" # get album tracks extension 
+    urlextension = "albums/$(album_id)/tracks?limit=$(limit)&offset=$(offset)&market=$(market)" # get album tracks extension 
 
-    spotify_request(spotifyDetails, urlextention) # make request
+    spotify_request(spotifyDetails, urlextension) # make request
 
 end # function get_album_tracks
 
@@ -59,9 +59,9 @@ Required scope: user-library-read
 
 """
 function get_saved_albums(spotifyDetails::SpotifyDetails; limit::Int=50, market::String="US", offset::Int=0)
-    urlextention = "me/albums?limit=$(limit)&offset=$(offset)&market=$(market)" # get saved albums extension 
+    urlextension = "me/albums?limit=$(limit)&offset=$(offset)&market=$(market)" # get saved albums extension 
 
-    spotify_request(spotifyDetails, urlextention) # make request
+    spotify_request(spotifyDetails, urlextension) # make request
 
 end # function get_saved_albums
 
@@ -75,9 +75,9 @@ function save_albums(spotifyDetails::SpotifyDetails, album_ids::Vector{String})
 
     album_ids_uri = HTTP.escapeuri(album_idsString) # perform url encoding 
 
-    urlextention = "me/albums?ids=$(album_ids_uri)" # get saved albums extension
+    urlextension = "me/albums?ids=$(album_ids_uri)" # get saved albums extension
 
-    spotify_request(spotifyDetails, urlextention; method="PUT") # make request
+    spotify_request(spotifyDetails, urlextension; method="PUT") # make request
 
 end # function save_album
 
@@ -91,9 +91,9 @@ function delete_albums(spotifyDetails::SpotifyDetails, album_ids::Vector{String}
 
     album_ids_uri = HTTP.escapeuri(album_idsString) # perform url encoding 
 
-    urlextention = "me/albums?ids=$(album_ids_uri)" # get delete albums extension (same as to save, but for DELETE request)
+    urlextension = "me/albums?ids=$(album_ids_uri)" # get delete albums extension (same as to save, but for DELETE request)
 
-    spotify_request(spotifyDetails, urlextention; method="DELETE") # make request
+    spotify_request(spotifyDetails, urlextension; method="DELETE") # make request
 
 end # function delete_albums
 
@@ -108,9 +108,9 @@ function check_saved_albums(spotifyDetails::SpotifyDetails, album_ids::Vector{St
 
     album_ids_uri = HTTP.escapeuri(album_idsString) # perform url encoding 
 
-    urlextention = "me/albums/contains?ids=$(album_ids_uri)" # get check saved albums extension
+    urlextension = "me/albums/contains?ids=$(album_ids_uri)" # get check saved albums extension
 
-    spotify_request(spotifyDetails, urlextention) # make request
+    spotify_request(spotifyDetails, urlextension) # make request
 
 end # function check_saved_albums
 
@@ -121,8 +121,9 @@ Required scope: ?
 
 """
 function get_new_releases(spotifyDetails::SpotifyDetails; limit::Int=50, country::String="US", offset::Int=0)
-    urlextention = "browse/new-releases?limit=$(limit)&country=$(country)&offset=$(offset)" # get new releases extension
+    urlextension = "browse/new-releases?limit=$(limit)&country=$(country)&offset=$(offset)" # get new releases extension
 
-    spotify_request(spotifyDetails, urlextention) # make request
+    spotify_request(spotifyDetails, urlextension) # make request
 
 end # function get_new_releases
+
