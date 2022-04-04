f1788d875913cbed7dbd47663f3e85959590dc65
Theo Rode <theorodester@gmail.com>
2022-04-04 10:26:26 -0700

    added search file. Not implemented yet, however file is there for when this happens

---
+++ src/search.jl
@@ -0,0 +1,14 @@
+"""
+Search for a particular 'item.' Get information from things on Spotify: 
+albums, artists, playlists, tracks, shows, or episodes:
+https://developer.spotify.com/documentation/web-api/reference/#/operations/search
+
+"""
+function search(spotifyDetails::SpotifyDetails)
+    # urlextension = "search?" # get url extension
+    
+    # spotify_request(spotifyDetails, urlextension) # make request
+
+
+    throw("Unimplemented")
+end # function search
