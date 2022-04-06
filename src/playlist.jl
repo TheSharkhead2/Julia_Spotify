88ba9aa4f4021864f13d8fe0e3b438f31b8dd369
Theo Rode <theorodester@gmail.com>
2022-04-06 08:04:13 -0700

    added add_items_to_playlist. Doesn't use request body meaning potential errors with large amounts of uris passed

--- src/playlist.jl
+++ src/playlist.jl
@@ -22,10 +22,33 @@ function get_playlist_items(spotifyDetails::SpotifyDetails, playlist_id::String;
 
     for arg in kwargs
         if arg[1] ∈ validKwargs 
-            url_extension *= "$(arg[1])=$(arg[2])"
+            url_extension *= "$(arg[1])=$(arg[2])&"
         end # if 
     end # for
 
     spotify_request(spotifyDetails, url_extension) # make request
 
 end # function get_playlist_items
+
+"""
+Adds items to a playlist:
+https://developer.spotify.com/documentation/web-api/reference/#/operations/add-tracks-to-playlist
+Requires scope: playlist-modify-private
+
+"""
+function add_items_to_playlist(spotifyDetails::SpotifyDetails, playlist_id, ids::Vector{String}; kwargs...)
+    ids_string = join(ids, ",") # join vector into string
+
+    ids_uri = HTTP.escapeuri(ids_string) # perform url encoding
+
+    url_extension = "playlists/$(playlist_id)/tracks?uris=$(ids_uri)" # get url extension
+
+    for arg in kwargs 
+        if arg[1] == :position # only valid other argument is position 
+            url_extension *= "&$(arg[1])=$(arg[2])"
+        end # if 
+    end # for
+
+    spotify_request(spotifyDetails, url_extension; method="POST") # make request
+
+end # function add_items_to_playlist
--- test/playlistTest.jl
+++ test/playlistTest.jl
@@ -7,10 +7,12 @@ using DotEnv
 DotEnv.config()
 
 const redirect_uri = "http://localhost:8888/callback"
-const scope = "user-read-private user-read-email "
+const scope = "user-read-private user-read-email playlist-modify-private"
 
 spotifyDetails = pkce_authorization(ENV["CLIENT_ID"], redirect_uri, scope)
 
 # println(get_playlist(spotifyDetails, "76KdKOh23Ofs73GtWZRlNQ"))
 
 # println(get_playlist_items(spotifyDetails, "76KdKOh23Ofs73GtWZRlNQ", limit=3))
+
+println(add_items_to_playlist(spotifyDetails, "7EWbd3NoGjjvnyS77ghUjl", ["spotify:track:6CX899yO5b5DZh2Gvtw5se"]))
